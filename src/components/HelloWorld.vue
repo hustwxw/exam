162b5fda2e4896eed8890a29cde041c3dd146669
solomonwu <softwarewxw@gmail.com>
2020-10-21 16:13:29 +0800

    fix

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,102 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <el-row>
+    <el-col :span="3" style="margin-right: 20px">
+      <el-select v-model="type" placeholder="请选择" @change="onSearch(true)">
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value">
+        </el-option>
+      </el-select>
+    </el-col>
+    <el-col :span="8">
+      <el-input v-model="keyword" placeholder="请输入题干搜索" clearable @keyup.native="onSearch" @clear="onSearch()"></el-input>
+    </el-col>
+    <el-col :span="4" style="margin-left: 20px">
+      <el-button type="primary" @click="onSearch">查询</el-button>
+    </el-col>
+    <el-col :span="24" style="margin-top: 20px;width: 100%">
+      <el-table
+        border 
+        :data="result"
+        style="width: 100%"
+        v-loading="loading"
+      >
+        <el-table-column
+          label="序号"
+          type="index"
+        >          
+        </el-table-column>
+        <el-table-column
+          prop="ques"
+          label="题干"
+          width="500px"
+          fit
+        >
+        </el-table-column>
+        <el-table-column
+          label="答案"
+          fit
+        >
+          <template slot-scope="scope">
+            <div v-for="(ele, index) in scope.row.answer" :key="ele" style="margin: 0 10px 10px 0">
+              <el-tag v-if="ele.indexOf('注意') === -1">
+                {{type === 0 ? `${index + 1}、` : null}}{{ele}}
+              </el-tag>
+              <el-tag v-else type="danger">
+                {{ele}}
+              </el-tag>
+            </div>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
+import mutiple from '../data/mutiple'
+import radio from '../data/radio'
+import tf from '../data/tf'
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data () {
+    return {
+      keyword: '',
+      result: [],
+      loading: false,
+      type: 0,
+      options: [
+        {
+          value: 0,
+          label: '多选'
+        },
+        {
+          value: 1,
+          label: '单选'
+        },
+        {
+          value: 2,
+          label: '判断'
+        }
+      ]
+    }
+  },
+  methods: {
+    onSearch (flag) {
+      if (flag) {
+        this.result = []
+      }
+      this.loading = true
+      setTimeout(() => {
+        this.result = [mutiple, radio, tf][this.type].filter(e => (e.ques || '').indexOf(this.keyword) > -1)
+        this.loading = false
+      }, 0)
+    }
+  },
+  mounted () {
+    this.result = mutiple
   }
 }
 </script>
